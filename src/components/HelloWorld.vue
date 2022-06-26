378c80f768adefef889df47490624eece2a73684
aczeeker <mr.arthurchoi@gmail.com>
2022-06-26 06:30:31 -0400

    1 file modified, 1 added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0);
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count += 2">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -36,5 +36,58 @@ const count = ref(0);
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+</style>
+
+<style scoped type="text/css">
+.wrapper {
+  background: #fff;
+  .footer {
+    color: #666;
+    text-align: center;
+    margin-top: 150px;
+    width: 100%;
+    span {
+      color: #1e90ff;
+      cursor: pointer;
+    }
+  }
+
+  .login {
+    .top {
+      margin: 20% 0 0 8%;
+      font-size: 26px;
+      span {
+        margin-left: 20px;
+        position: relative;
+        bottom: 5px;
+        color: #666;
+      }
+    }
+
+    .form {
+      margin: 10% 8% 0 8%;
+      input {
+        font-family: 'Microsoft Yahei';
+        width: 100%;
+        &.btn {
+          line-height: 32px;
+          background: #1e90ff;
+          color: white;
+          margin-top: 50px;
+        }
+      }
+      a {
+        margin-top: 20px;
+        color: #1e90ff;
+      }
+      ::-webkit-input-placeholder,
+      ::-moz-placeholder,
+      ::-moz-placeholder,
+      ::-ms-input-placeholder {
+        color: #333;
+      }
+    }
+  }
 }
 </style>
